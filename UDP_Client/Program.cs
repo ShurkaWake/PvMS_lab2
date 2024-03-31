dcbd13bc0ddadd403bf225ac2ce5642bc966b76e
ShurkaWake <a.a.prosud@gmail.com>
2024-03-31 14:25:47 +0300

    Updated UDP server port

--- UDP_Client/Program.cs
+++ UDP_Client/Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-const int port = 5000;
+const int port = 5112;
 const string ip = "127.0.0.1";
 
 try
--- UPD_Server/Program.cs
+++ UPD_Server/Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-const int port = 5000;
+const int port = 5112;
 UdpClient udpServer = null;
 
 try
